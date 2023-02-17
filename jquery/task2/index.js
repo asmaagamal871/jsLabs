2604c908348c21878d6d0ab0b950aa382aa138c6
asmaa gamal <asmaagamal99192@gmail.com>
2023-02-18 01:49:21 +0200

    jQuery lab1

---
+++ jquery/task2/index.js
@@ -0,0 +1,9 @@
+$("#1").click(function(){
+    $("#p1").toggle(2000);
+  });
+  $("#2").click(function(){
+    $("#p2").toggle(2000);
+  });
+  $("#3").click(function(){
+    $("#p3").toggle(2000);
+  });
